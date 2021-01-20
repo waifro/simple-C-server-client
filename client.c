b14c0ff2e771d38eccf54232c3ddfdfee6f8569c
richardwaifro <77700999+richardwaifro@users.noreply.github.com>
2021-01-21 03:26:06 +0500

    managed to establish connection

--- client.c
+++ client.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 #include <winsock2.h>
 #include <string.h>
+#include <unistd.h>
 
-#define PORT "2705"
+// CLIENT
+
+#define PORT 5001
 
 // using -lws2_32 on linker
 // https://docs.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
 
 int main(int argc, char *argv[]) {
 
-    int sockfd = 0;
-    int portno = 0;
+    int sockfd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
+    char buffer[256] = "ciao!";
     char buf[256];
 
     WSADATA wsaData;
-
     int res = WSAStartup(MAKEWORD(2,2), &wsaData);
 
     if (res < 0) {
@@ -27,66 +28,74 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    portno = atoi(PORT);
-
     // Create a socket point
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    printf("sockfd %d\n", sockfd);
 
     if (sockfd < 0) {
         printf("Error opening socket: %d", WSAGetLastError());
         exit(2);
     }
 
-    gethostname(buf, 256);
+    res = gethostname(buf, 256);
+
+    if (res < 0) {
+        printf("Error hostname: %d", WSAGetLastError());
+        exit(3);
+    }
+
+    printf("found: %s\n", buf);
     server = gethostbyname(buf);
 
     if (server == NULL) {
         printf("Error host: %d", WSAGetLastError());
-        exit(3);
+        exit(4);
     }
 
-    memset((char *)&serv_addr, 0x00, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    memcpy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr = *(struct in_addr *)*server -> h_addr_list;
+    serv_addr.sin_port = htons(PORT);
 
     // Now connect to the server
     res = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
+    printf("serv_addr %d\n", (int)sizeof(serv_addr));
+
     if (res < 0) {
        printf("Error connect socket: %d", WSAGetLastError());
-        exit(4);
+        exit(5);
     }
 
     // Now ask for a message from the user, this message
     // will be read by server
 
-    printf("Please enter the message: ");
-    memset(buffer, 0x00, 256);
-    fgets(buffer, 255, stdin);
-
-    printf("Message got it\n");
+    printf("Please enter the message: ciao!\n");
+    //memset(buffer, 0x00, 256);
+    //fgets(buffer, 255, stdin);
 
     // Send message to the server
-    res = memcpy(&sockfd, buffer, sizeof(buffer));
-    //res = fwrite(&sockfd, sizeof(char), sizeof(buffer), buffer);
+    res = mempcpy(&sockfd, buffer, sizeof(sockfd));
 
     if (res < 0) {
        printf("Error writing socket: %d", WSAGetLastError());
-        exit(5);
-    }
-
-    printf("Message sent\n");
-
-    // Now read server response
-    memset(buffer, 0x00, 256);
-    res = fread(buffer, sizeof(char), sizeof(sockfd), (FILE*)sockfd);
-
-    if (res < 0) {
-        printf("Error reading socket: %d", WSAGetLastError());
         exit(6);
     }
 
-    printf("%s\n", buffer);
+    printf("\nMessage sent\n");
+
+    // Now read server response
+    //memset(buffer, 0x00, 256);
+    //res = mempcpy(buffer, (void*)&sockfd, sizeof(sockfd));
+    //res = mempcpy(buffer, sockfd, sizeof(sockfd));
+
+    res = read(sockfd, buffer, sizeof(buffer));
+
+    if (res < 0) {
+        printf("Error reading socket: %d", WSAGetLastError());
+        exit(7);
+    }
+
+    printf("Message received: %s\n", buffer);
     return 0;
 }
