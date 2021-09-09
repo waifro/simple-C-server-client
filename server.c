9cb286f2fb7858e3bf531f95aac6a4f73ac687e7
richardwaifro <77700999+richardwaifro@users.noreply.github.com>
2021-09-09 19:48:11 +0000

    added support for linux

--- server.c
+++ server.c
@@ -1,36 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <winsock2.h>
 #include <string.h>
 #include <unistd.h>
+
+#ifdef _WIN32
+	#include <winsock2.h>
+#else
+	#include <netinet/in.h>
+	#include <sys/socket.h>
+	#include <sys/types.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
+#endif
+
 #define PORT 5001
 
 // SERVER
 // using -lws2_32 on linker
 
 int main(void) {
+    
+    int result = 0;
 
-    // initializing windows socket
-    WSADATA WsaData;
-    int result = WSAStartup(MAKEWORD(2,2), &WsaData);
-    if (result < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    #ifdef _WIN32
+		// initializing windows socket
+		WSADATA WsaData;
+		result = WSAStartup(MAKEWORD(2,2), &WsaData);
+		if (result < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    #endif
 
     int sockfd;
 
     struct sockaddr_in server;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    if (sockfd < 0) {
+        
+        #ifdef _WIN32
+	     printf("Errid: %d", WSAGetLastError()); exit(1);
+        #else
+	     perror("Errid: "); exit(1);
+        #endif
+	
+    }
 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(PORT);
 
     result = bind(sockfd, (struct sockaddr*)&server, sizeof(server));
-    if (result < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    if (result < 0) {
+        
+        #ifdef _WIN32
+	     printf("Errid: %d", WSAGetLastError()); exit(1);
+        #else
+	     perror("Errid: "); exit(1);
+        #endif
+	
+    }
 
     result = listen(sockfd, 5);
-    if (result < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    if (result < 0) {
+        
+        #ifdef _WIN32
+	     printf("Errid: %d", WSAGetLastError()); exit(1);
+        #else
+	     perror("Errid: "); exit(1);
+        #endif
+	
+    }
 
     // while the socket sockfd is listening, establish a new connection
 
@@ -38,20 +76,40 @@ int main(void) {
 
     int newsockfd;
     newsockfd = accept(sockfd, NULL, NULL);
-    if (newsockfd < 0) { printf("Errid: %d", WSAGetLastError()); exit(1); }
+    if (newsockfd < 0) {
+        
+        #ifdef _WIN32
+	     printf("Errid: %d", WSAGetLastError()); exit(1);
+        #else
+	     perror("Errid: "); exit(1);
+        #endif
+	
+    }
 
     printf("Connection from socket id: %d\n", newsockfd);
 
     char buf[256];
 
     result = recv(newsockfd, buf, sizeof(buf), 0);
-    if (result < 0) { perror("Errno: "); exit(1); }
+    if (result < 0) {
+        
+        #ifdef _WIN32
+	     printf("Errid: %d", WSAGetLastError()); exit(1);
+        #else
+	     perror("Errid: "); exit(1);
+        #endif
+	
+    }
 
     printf("msg received: %s\n", buf);
 
     close(newsockfd);
     close(sockfd);
-    WSACleanup();
+    
+	#ifdef _WIN32
+		WSACleanup();
+	#endif
+
     return 0;
 
 }
